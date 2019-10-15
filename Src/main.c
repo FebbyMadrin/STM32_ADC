737dda7ab6515b1d4ca4dbff31c086b39d27f785
Febby Purnama Madrin <febbymadrin@gmail.com>
2019-10-15 16:45:59 +0200

    no option enabled

--- Src/main.c
+++ Src/main.c
@@ -132,7 +132,7 @@ int main(void)
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
   //SET_ADCOption1 (ENABLE);
- SET_ADCOption2 (SYSCFG_PMC_ADCxDC2, ENABLE);
+ //SET_ADCOption2 (SYSCFG_PMC_ADCxDC2, ENABLE);
   /* USER CODE END 2 */
 
   /* Infinite loop */
